b70c1cec95f47c3adf966cc6d6f72edda997c1a7
Estefani Moré <iurymarmore@gmail.com>
2020-05-11 21:00:17 -0300

    resumos

---
+++ resumoJS.js
@@ -0,0 +1,39 @@
+/*console.log(typeof 'hello word'); string
+console.log(typeof 'pruuuu'); string
+console.log(typeof 45) numero
+*/
+
+//operadores
+/*console.log(2+2)
+console.log(2*4-3)
+console.log(8/4)
+console.log(5+(2*4))
+*/
+
+//Special Numbers
+ /*console.log(typeof Infinity)
+ console.log(typeof -Infinity)
+ console.log(typeof NaN)*/
+
+ //Strings
+ /*console.log(typeof 'hello word')
+ console.log(typeof "pruuu")
+ console.log(typeof `template literals`) //adiciona variaveis no texto com crase*/
+
+ //Curiosidades Strings
+ /*console.log('Primeira linha \n Segunda linha')
+ console.log(`soma de 2 + 2 é ${2+2}`) serve para identificar string mas também para computar valores
+ console.log('salada ' + 'de ' + 'fruta')*/
+
+ //Bolean operadores lógicos
+ //&& - and = para ser true os dois lados da comparação precisam ser true // console.log(5 > 3 && 3 == 2);
+ //|| - or = um ou outro precisam ser verdadeiro // console.log(5 > 3 || 3 == 2);
+ // !  - not = inverte valores(true vira false) // console.log(5 > 3 != 3 == 2);
+
+ //Operadores ternários
+ console.log(typeof true ? 1 : 2);
+ console.log(typeof false ? 'false' : 'true');
+
+
+
+
